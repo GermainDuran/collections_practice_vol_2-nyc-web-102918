d333739acd3571c0408261f95f9225e917b8701c
GERMAIN ARTURO DURAN TORRES <arturodurantorres@gmail.com>
2018-10-17 16:37:25 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,17 +45,17 @@ def remove_non_strings(array)
   }
 end 
 
-#def count_elements(array)
-  #i=0 
-  #c=0 
-  #while (i<array.length){
-    #if array[i] != nil 
-     # c+=1
-    #end
-   # i+=1
-  #}
- # return c
-#end
+def count_elements(array)
+  i=0 
+  c=0 
+  while (i<array.length){
+    if array[i] != nil 
+      c+=1
+    end
+    i+=1
+  }
+  
+end
 
 def merge_data(array, data)
   i = 0
